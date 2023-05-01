a66f16569065c2ae439e02202e62b59e8a3a63f9
Nadav Dayan <ndayan@appdev.yearup.org>
2023-05-01 10:50:05 -0400

    made the layout of the submenus and created the construstors

---
+++ src/main/java/com/accounting/AccountingEntries.java
@@ -0,0 +1,65 @@
+package com.accounting;
+
+import java.util.Date;
+
+public class AccountingEntries {
+    //Format: date|time|description|vendor|amount
+    private Date date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public AccountingEntries(Date date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+    }
+}
--- src/main/java/com/accounting/AccountingLedger.java
+++ src/main/java/com/accounting/AccountingLedger.java
@@ -26,10 +26,70 @@ public class AccountingLedger {
 
             switch (command) {
                 case "D": //Add a deposit
+                    //Prompt the user to add an entry
+                    //Add it to the csv file
                     break;
                 case "P": //Make a Payment (Debit)
+                    //Prompt the user for the debit information
+                    //Add it to the csv file
                     break;
                 case "L": //Ledger
+                    System.out.println("\tYour Ledger");
+                    System.out.println("A. Display all entries");
+                    System.out.println("D. Display all deposits");
+                    System.out.println("P. Displays all payments");
+                    System.out.println("R. Run a report");
+                    System.out.println("H. Return to Home screen");
+                    System.out.print("What would you like to do: ");
+                    String command2 = input.nextLine();
+                    input.nextLine();
+                    switch (command2){
+                        case "A": //Display all entries
+                            break;
+                        case "D": //Displays deposits
+                            break;
+                        case "P": //Displays payments
+                            break;
+                        case "R": //Run a report
+                            System.out.println("Reports:");
+                            System.out.println("\t1. Month to Date");
+                            System.out.println("\t2. Previous Month");
+                            System.out.println("\t3. Year to Date");
+                            System.out.println("\t4. Previous Year");
+                            System.out.println("\t5. Search by vendor");
+                            System.out.println("\t0. Return to the Ledger");
+                            System.out.print("Choose a report: ");
+                            int command3 = input.nextInt();
+                            while (command3 != 0) {
+                                switch (command3){
+                                    case 1: //Month to Date
+                                        break;
+                                    case 2: //Previous Month
+                                        break;
+                                    case 3: //Year to Date
+                                        break;
+                                    case 4: //Previous Year
+                                        break;
+                                    case 5: //Search by vendor
+                                        //Display current list of vendors
+                                        //prompt a selection
+                                        break;
+                                    case 0: //return to ledger
+                                        System.out.println("Returning to Ledger menu");
+                                        return;
+                                    default: //User Error
+                                        System.out.println("Invalid input, try again.");
+                                        break;
+                                }
+                            }
+                            break;
+                        case "H": //Return to the home screen
+                            System.out.println("Returning to home screen.");
+                            return;
+                        default:
+                            System.out.println("Invalid input, try again.");
+                            break;
+                    }
                     break;
                 case "X": //Exit
                     System.out.println("Thank you. Have a Good Day!");
